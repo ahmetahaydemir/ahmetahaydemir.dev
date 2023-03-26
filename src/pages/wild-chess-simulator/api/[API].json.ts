e9b810fa84820f6dd072ae312d5a4d9d8d5d15a7
ahmetahaydemir <ahmetahaydemir@gmail.com>
2023-03-26 17:26:13 +0300

    Static File Endpoints - API Route

---
+++ src/pages/wild-chess-simulator/api/[API].json.ts
@@ -0,0 +1,43 @@
+import type { APIRoute } from 'astro';
+import { skillDataJsonArray } from '../../../../public/data/SkillDataJsonFile.json';
+import { classDataJsonArray } from '../../../../public/data/ClassDataJsonFile.json';
+import { originDataJsonArray } from '../../../../public/data/OriginDataJsonFile.json';
+import { entityDataJsonArray } from '../../../../public/data/EntityDataJsonFile.json';
+import { equipmentDataJsonArray } from '../../../../public/data/EquipmentDataJsonFile.json';
+
+export const get: APIRoute = ({ params, request }) => {
+    const API = params.API;
+    let jsonData = { Message: "Invalid JSON Data Request" };
+    //
+    switch (API) {
+        case "getEntityData":
+            jsonData = { entityDataJsonArray };
+            break;
+        case "getClassData":
+            jsonData = { classDataJsonArray };
+            break;
+        case "getOriginData":
+            jsonData = { originDataJsonArray };
+            break;
+        case "getSkillData":
+            jsonData = { skillDataJsonArray };
+            break;
+        case "getEquipmentData":
+            jsonData = { equipmentDataJsonArray };
+            break;
+    }
+    //
+    return {
+        body: JSON.stringify(jsonData)
+    }
+};
+
+export function getStaticPaths() {
+    return [
+        { params: { API: "getEntityData" } },
+        { params: { API: "getClassData" } },
+        { params: { API: "getOriginData" } },
+        { params: { API: "getSkillData" } },
+        { params: { API: "getEquipmentData" } }
+    ]
+}
