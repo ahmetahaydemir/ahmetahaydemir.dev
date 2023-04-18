4b8207fb26c45da0195b007e013d7c3dd5ef47c8
Ahmet Taha Aydemir <ahmetahaydemir@gmail.com>
2023-04-18 12:48:48 +0300

    Only Dark Theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact({
-    compat: true
-  }), mdx(), tailwind()]
+	integrations: [mdx(), tailwind()]
 });
