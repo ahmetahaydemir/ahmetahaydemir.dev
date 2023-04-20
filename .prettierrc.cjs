28cf2d68ef97798b93977cc264109314fd040e3f
ahmetahaydemir <ahmetahaydemir@gmail.com>
2023-04-20 13:10:41 +0300

    Prettier Config And Draggable Start

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+  arrowParens: "always",
+  printWidth: 150,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  tabWidth: 2,
+};
+
+module.exports = config;
