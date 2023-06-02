e8cd24a381d7fe767bc79f4d9c0034fe6f96958d
Ahmet Taha Aydemir <ahmetahaydemir@gmail.com>
2023-06-03 01:18:07 +0300

    Socket IO & Node JS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind()]
+  integrations: [mdx(), tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
